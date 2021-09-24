ea30f9c683fb15901c7ec474e6e631a9cc2fc5b3
Code4Wyatt <44061707+Code4Wyatt@users.noreply.github.com>
2021-09-24 15:18:58 +0100

    Update

--- M2D5-main/script.js
+++ M2D5-main/script.js
@@ -51,7 +51,7 @@ const getMovies = async () => {
                             ${chunk.map((movie) => `<div class="col-sm-4 col-lg-1 movie-cards">
                                     <img class="movie-cover" src="${movie.imageUrl}">
                                     <h6 class="movieName">${movie.name}</h6>
-                                    <a class="info" href="#"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-square-fill info" viewBox="0 0 16 16">
+                                    <a class="info" href="#" ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-info-square-fill info" viewBox="0 0 16 16">
   <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm8.93 4.588-2.29.287-.082.38.45.083c.294.07.352.176.288.469l-.738 3.468c-.194.897.105 1.319.808 1.319.545 0 1.178-.252 1.465-.598l.088-.416c-.2.176-.492.246-.686.246-.275 0-.375-.193-.304-.533L8.93 6.588zM8 5.5a1 1 0 1 0 0-2 1 1 0 0 0 0 2z"/>
 </svg></a>
                                     <a id="edit" class="btn btn-warning" href="backoffice.html?id=${movie._id}">Edit</a>
@@ -84,3 +84,4 @@ const getMovies = async () => {
         console.error(error)
     }
 }
+
